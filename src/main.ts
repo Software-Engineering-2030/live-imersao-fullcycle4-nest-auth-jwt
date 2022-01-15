a3a034fac8069bed86b68e7e73332e68d3c13a2f
Andre Souza <viannaandresouza@gmail.com>
2022-01-15 00:32:26 -0300

    CORS Update

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors:true});
+  // app.enableCors();
   await app.listen(3000);
 }
+
+// const app = await NestFactory.create(AppModule);
+// app.enableCors();
+// await app.listen(3000);
 bootstrap();
